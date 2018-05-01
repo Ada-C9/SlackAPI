049f37e3c1581af0678380e676c2ef003a1a00ed
Dan Roberts <dan@adadevelopersacademy.org>
2018-05-01 16:51:25 -0700

    In class work

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,3 +1,14 @@
 class Channel
+  # Add more fields as needed
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
 
+  # This is a factory method! It reads the data
+  # we got back from the API, and turns it into
+  # an instance of Channel by calling self.new
+  def self.from_api(raw_channel)
+    self.new(raw_channel["name"])
+  end
 end
--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -1,5 +1,41 @@
-require 'httparty'
+# require 'httparty'
+require 'pry'
 
 class SlackApiWrapper
-  # Your code here!
+  class SlackError < StandardError; end
+
+  def self.list_channels
+    token = ENV["SLACK_API_TOKEN"]
+    url = "https://slack.com/api/channels.list?token=#{token}"
+    response = HTTParty.get(url)
+
+    # ALWAYS CHECK YOUR ERROR CODES
+    raise_on_error(response)
+
+    return response["channels"].map do |raw_channel|
+      Channel.from_api(raw_channel)
+    end
+  end
+
+  def self.send_message(channel_name, message)
+    token = ENV["SLACK_API_TOKEN"]
+    url_root = "https://slack.com/api/chat.postMessage"
+    # Construct the full URL from the endpoint and the query params
+    full_url = URI.encode("#{url_root}?channel=#{channel_name}&text=#{message}&token=#{token}")
+
+    puts "About to send request to #{full_url}"
+
+    response = HTTParty.post(full_url)
+
+    raise_on_error(response)
+
+    # No return needed
+  end
+
+  private
+  def raise_on_error(resposne)
+    unless response["ok"]
+      raise SlackError.new(response["error"])
+    end
+  end
 end
